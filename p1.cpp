78f8ec348d02caa0b6bf82c497aaa3140ea8129c
a0952866367 <a0952866367@yahoo.com.tw>
2015-04-04 18:27:26 +0800

    ADD:bool CheckUnity

--- p1.cpp
+++ p1.cpp
@@ -3,19 +3,39 @@ using namespace std;
 
 class sudoku{
 public:
-	static const int size = 12;
-	int map[size][size] = {0};
+	static const int size = 144;
 	
+	bool CheckMap(int all[]){
+		
+	}
 	void GiveQuestion();
 	void ReadIn(){
-		for( int i = 0 ; i < 9 ; i++ ){
-            		for( int j = 0 ; j < 9 ; j++ ){
-                		cin >> sudoku[i][j];
+		for( int i = 0 ; i < 12 ; i++ ){
+            		for( int j = 0 ; j < 12 ; j++ ){
+                		cin >> sudoku[i];
 			}
 		}
 	}
 	void Solve();
-	
-	
+private:
+	int map[size] = {};
+	bool CheckUnity(int unit[]){
+		int amount[12] = {};//count unity numbers(set to 0)
+		for( int i=0 ; i<12 ; i++ ){
+			unit[i] = all[i];
+		}
+		for( int i=0 ; i<12 ; i++ ){
+			amount[ unit[i]+1 ]++;
+		}
+		if( amount[0]!=3 ){
+			return false;
+		}
+		for( int i=2 ; i<=10 ; i++ ){
+			if( amount[i]!=1 ){
+				return false;
+			}
+		}
+		return true;
+	}
 	
 }
